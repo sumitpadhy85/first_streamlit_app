ec134f090415b8377ec0d403862c1dc0aed061a8
sumitpadhy85 <143688305+sumitpadhy85@users.noreply.github.com>
2023-09-08 23:10:19 +0530

    Update streamlit_app.py
    c1

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,3 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
